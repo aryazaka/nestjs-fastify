d8453b68b5b03145649ef15b7aa08e461a15b7eb
aryazaka <aryapurnomo07@gmail.com>
2025-07-15 16:00:31 +0700

    test 4

--- worker/test-ws.js
+++ worker/test-ws.js
@@ -10,7 +10,7 @@ socket.on('connect', () => {
 socket.on('company_created', (data) => {
   console.log('Received created company from WS:', data);
 }); 
-console.log('test 3');
+console.log('test 4');
 socket.on('disconnect', () => {
   console.log('Disconnected');
 });
