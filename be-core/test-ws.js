499e2f8fd4d0fee783af01871a407f8961881fc0
aryazaka <aryapurnomo07@gmail.com>
2025-07-15 15:30:02 +0700

    test 1

--- be-core/test-ws.js
+++ be-core/test-ws.js
@@ -10,7 +10,7 @@ socket.on('connect', () => {
 socket.on('company_created', (data) => {
   console.log('Received created company from WS:', data);
 }); 
-
+console.log('test 1');
 socket.on('disconnect', () => {
   console.log('Disconnected');
 });
